522730bd5c8248a1a8be7832004ea996ca6ba7c8
Kristina <kristina20052020@gmail.com>
2025-04-29 15:04:23 +0300

    lab 9 task 2
    Найти количество цифр числа, меньших 3

--- Recursion.pl
+++ Recursion.pl
@@ -1,3 +1,4 @@
+%Найти минимальную цифру числа.
 find_min_digit_down(Number, MinDigit) :-
     find_min_digit_down_acc(Number, 9, MinDigit).
 
@@ -17,3 +18,28 @@ find_min_digit_up(Number, MinDigit) :-
     NextNumber is Number // 10,
     find_min_digit_up(NextNumber, RestMin),
     MinDigit is min(Digit, RestMin).
+
+
+%Найти количество цифр числа, меньших 3
+% count_digits_down(+Number, -Count)
+count_digits_down(Number, Count) :-
+    count_digits_down_acc(Number, 0, Count).
+
+count_digits_down_acc(0, Count, Count) :- !.
+
+count_digits_down_acc(Number, CurrentCount, FinalCount) :-
+    Digit is Number mod 10,
+    (Digit < 3 -> NewCount is CurrentCount + 1 ; NewCount is CurrentCount),
+    NextNumber is Number // 10,
+    count_digits_down_acc(NextNumber, NewCount, FinalCount).
+
+
+% count_digits_up(+Number, -Count)
+count_digits_up(0, 0) :- !.
+
+count_digits_up(Number, Count) :-
+    Digit is Number mod 10,
+    NextNumber is Number // 10,
+    count_digits_up(NextNumber, RestCount),
+    (Digit < 3 -> Count is RestCount + 1 ; Count is RestCount).
+
